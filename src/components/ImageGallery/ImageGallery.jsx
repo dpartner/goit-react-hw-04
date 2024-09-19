26658ee466b0acf4d3ac9e1fec0ccc51fc3755ec
dpartner <d.partner.ua@gmail.com>
2024-09-19 18:03:43 +0300

    finaly

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,19 @@
+import ImageCard from '../ImageCard/ImageCard'
+import s from './ImageGallery.module.css'
+
+
+const ImageGallery = ({request, openModal}) => {
+  console.log(request)
+  return (
+    <ul className={s.photoList}>
+      {request.map(item=> (
+        <li className={s.photoItme} key={item.id}>
+        <ImageCard {...item} openModal={openModal}/>
+      </li>)
+      )}
+    </ul>
+    
+  )
+}
+
+export default ImageGallery
